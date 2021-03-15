f025499aa20d2c6262f062357cae9e6c2ebcd12a
Jon Stump <jmstump@gmail.com>
2021-03-15 10:59:21 -0700

    add tests
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_173545) do
+ActiveRecord::Schema.define(version: 2021_03_15_175219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_03_15_173545) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "sightings", "animals"
 end
